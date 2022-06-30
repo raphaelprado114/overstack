207032acc25750da7171938c0ad8cb5786a743a0
Vincenzo de Luca <61676750+Jsprofile@users.noreply.github.com>
2022-06-30 17:46:34 -0300

    New Addons

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 var bx = document.querySelector('.bx')
 var mobileNav = document.querySelector('.mobileNav')
 
-bx.addEventListener('click', ()=>{
+bx.addEventListener('click', () => {
     bx.classList.toggle('activeBx')
     mobileNav.classList.toggle('activeMenu')
 })
@@ -11,48 +11,101 @@ bx.addEventListener('click', ()=>{
 // -----------------------------------------
 
 
-// Show menu Desktop on scroll 
+// Efects on scroll 
 
 var menuDesktop = document.querySelector('.header')
+var btnScroll = document.querySelector('.scrollTop')
+const target = document.querySelectorAll('[data-anime]')
+const animateClass = 'animate'
 
-window.onscroll = ()=>{
-    if(window.pageYOffset >= 100){
+window.onscroll = () => {
+
+    // Header
+
+    if (window.pageYOffset >= 100) {
         menuDesktop.classList.add('activeHeader')
-    }else{
+        btnScroll.classList.add('scrollTop-active')
+    } else {
         menuDesktop.classList.remove('activeHeader')
+        btnScroll.classList.remove('scrollTop-active')
     }
+
+
+    // Side Scrolling Animations
+
+    const windowTop = window.pageYOffset + 550
+    target.forEach((item) => {
+        if (windowTop > item.offsetTop) {
+            item.classList.add(animateClass)
+        }
+    })
 }
+
 
 // -----------------------------------------
 
 
 // Counter
 
-const countDate = new Date('july 15, 2022 00:00:01').getTime();
-function newYear() {
+// const countDate = new Date('july 15, 2022 00:00:01').getTime();
+// function newYear() {
 
-  const now = new Date().getTime();
-  const gap = countDate - now;
+//     const now = new Date().getTime();
+//     const gap = countDate - now;
 
-  const segundos = 1000;
-  const minutos = segundos * 60;
-  const hora = minutos * 60;
-  const dia = hora * 24;
+//     const segundos = 1000;
+//     const minutos = segundos * 60;
+//     const hora = minutos * 60;
+//     const dia = hora * 24;
 
-  const d = Math.floor(gap / (dia));
-  const h = Math.floor((gap % (dia)) / (hora));
-  const m = Math.floor((gap % (hora)) / (minutos));
-  const s = Math.floor((gap % (minutos)) / (segundos));
+//     const d = Math.floor(gap / (dia));
+//     const h = Math.floor((gap % (dia)) / (hora));
+//     const m = Math.floor((gap % (hora)) / (minutos));
+//     const s = Math.floor((gap % (minutos)) / (segundos));
 
-  document.getElementById('dia').innerText = d;
-  document.getElementById('hora').innerText = h;
-  document.getElementById('minutos').innerText = m;
-  document.getElementById('segundos').innerText = s;
+//     document.getElementById('dia').innerText = d;
+//     document.getElementById('hora').innerText = h;
+//     document.getElementById('minutos').innerText = m;
+//     document.getElementById('segundos').innerText = s;
 
-}
+// }
 
-setInterval(function () {
-  newYear();
-}, 1000);
+// setInterval(function () {
+//     newYear();
+// }, 1000);
 
 // -----------------------------------------
+
+
+// ScrollTop
+
+btnScroll.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
+
+
+
+
+  
+  function toggleAnswer(div) {
+    const divAnswer = div.nextElementSibling
+  
+    divAnswer.classList.toggle('active')
+  }
+  
+  
+// Popuper
+
+var btnPopWhite = document.querySelector('.popuperWhite')
+var advantagesPopup = document.querySelector('.advantages > .popuper')
+var bxP = document.querySelector('.bxP')
+
+
+btnPopWhite.addEventListener('click', ()=>{
+    advantagesPopup.classList.remove('fadeTopDown')
+    advantagesPopup.style.display = 'block'
+})
+
+bxP.addEventListener('click', ()=>{
+    advantagesPopup.classList.add('fadeTopDown')
+})
